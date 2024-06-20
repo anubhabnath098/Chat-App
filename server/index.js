bd4f7dbe73f76045203e3d9ba710290e9b2765de
anubhab-nath098 <anubhabnath19@gmail.com>
2024-06-20 17:23:13 +0530

    changes made

--- server/index.js
+++ server/index.js
@@ -15,14 +15,8 @@ const allowedOrigins = [
 ]
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or curl requests)
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
+    origin: allowedOrigins,
+    methods:["GET", "POST"],
     credentials: true
 }));
 
